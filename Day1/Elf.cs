71c7efcccee0a8877fd2b44d2636b11a5e294522
Yente Paternotte <yente.paternotte@hotmail.com>
2022-12-01 15:00:54 +0100

    day 1 completed

---
+++ Day1/Elf.cs
@@ -0,0 +1,18 @@
+﻿namespace Day1;
+
+public class Elf
+{
+    public List<long> Callories { get; set; }
+    public long MaxCallories
+    {
+        get
+        {
+            long total = Callories.Sum(x=> x);
+            return total;
+        }
+    }
+    public Elf()
+    {
+        Callories = new List<long>();
+    }
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Day1;
+class Program
+{
+    public static List<Elf> elfs { get; set; }
+    static void Main(string[] args)
+    {
+        elfs = new List<Elf>();
+        string[] lines = System.IO.File.ReadAllLines(@"./input.txt");
+        long total = 0;
+        Elf elf = new Elf();
+        for(int i = 0; i < lines.Length; i++ )
+        {
+            if (!string.IsNullOrEmpty(lines[i]))
+            {
+                long calories = long.Parse(lines[i]);
+                elf.Callories.Add(calories);
+            }
+            else
+            {
+                elfs.Add(elf);
+                elf = new Elf();
+            }
+        }
+        Console.WriteLine("The elf with the highest callories is: " + elfs.Max(e => e.MaxCallories));
+        Console.WriteLine("The top 3 elfs with the highest callories is: " + elfs.OrderByDescending(x => x.MaxCallories).Take(3).Sum(e => e.MaxCallories));
+    }
+}
