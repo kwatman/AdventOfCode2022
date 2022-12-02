37d1eba65bad986cad00dd08ac0404b01cb285a5
Yente Paternotte <yente.paternotte@hotmail.com>
2022-12-02 10:57:15 +0100

    done part 1

---
+++ Day2/Shape.cs
@@ -0,0 +1,34 @@
+﻿namespace Day2;
+
+public class Shape
+{
+    public static List<Shape> Shapes;
+    public string Name { get; set; }
+    public string OpponentAlias { get; set; }
+    public string Alias { get; set; }
+    public int Score { get; set; }
+    
+    public Shape Weakness { get; set; }
+
+    public Shape(string name,int score,string opponentAlias,string alias)
+    {
+        Name = name;
+        Score = score;
+        OpponentAlias = opponentAlias;
+        Alias = alias;
+        Shapes.Add(this);
+    }
+
+    public static Shape GetShape(string shapeAlias)
+    {
+        foreach (Shape shape in Shapes)
+        {
+            if (shape.Alias == shapeAlias || shape.OpponentAlias == shapeAlias)
+            {
+                return shape;
+            }
+        }
+        return null;
+    }
+
+}
